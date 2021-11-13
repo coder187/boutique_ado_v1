e0ccf83c9ea7566f6caf475b2865dd93f502894c
coder187 <coder187@hotmail.com>
2021-11-13 13:55:42 +0000

    added home app and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # urls for login/logout/password reset etc..
+    path('', include('home.urls')),
 ]
