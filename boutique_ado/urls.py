ddca1544007052ec644460116a3e609c66886ec4
coder187 <coder187@hotmail.com>
2021-11-13 15:01:30 +0000

    completed home page header and css

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static, settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # urls for login/logout/password reset etc..
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
