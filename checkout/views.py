2c630792b7eb09392000d836cf7af1fcc34be6a5
coder187 <coder187@hotmail.com>
2021-12-05 21:20:42 +0000

    update checkout init.py. add 'processing' overlay to checkout.

--- checkout/views.py
+++ checkout/views.py
@@ -102,7 +102,7 @@ def checkout(request):
                 amount=stripe_total,
                 currency=settings.STRIPE_CURRENCY,
                 )
-        # print(intent)
+        print(intent)
 
         order_form = OrderForm()
     
