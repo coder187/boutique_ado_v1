741e48ade7deb126827d1e0f64602599a450b6ad
coder187 <coder187@hotmail.com>
2021-12-06 13:19:17 +0000

    update checkout views imports

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
-import datetime
-
-
 import stripe
+import json
+import datetime
 
 @require_POST
 def cache_checkout_data(request):
